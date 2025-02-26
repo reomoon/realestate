de8e86b51b9704a31e2543826fa80ffedc05cfcd
reomoon <taehyung-moon@nhnservice.com>
2025-02-26 18:04:37 +0900

    v1.0 commit

---
+++ common.py
@@ -0,0 +1,47 @@
+# 요청에 필요한 쿠키와 헤더 설정
+cookie = {
+    'NNB': 'Q7SZXZGNQE6WM',
+    'ASID': 'afcf3ea90000018f7c2e0fc800000044',
+    'NFS': '2',
+    'NV_WETR_LAST_ACCESS_RGN_M': '"V0RKUE4wMDE3Nw=="',
+    'NV_WETR_LOCATION_RGN_M': '"V0RKUE4wMDE3Nw=="',
+    'tooltipDisplayed': 'true',
+    'NAC': 'FqSABcAfF1ydB',
+    'NACT': '1',
+    'nid_inf': '2053380545',
+    'NID_AUT': 'cjajOnHhfJAoxFybnsfEBj0Zj0mHhCThwjAbmIud9fBaFRjEMQp5ucWaAsDl56ij',
+    'NID_SES': 'AAABy0dlQrschR8u8Bm+vbM4bN7822qroZLOvKMwDMiKNTdOpHv51T3bgMes3CH35KiBj+3w9KxnucTmiUiTNZ/yFpMJ6US0u+DjoShLwJIAlOWjEPJP+Z20KYozPpsSWyanwsRyRXUxNT9iZ+eFG3X2XYZ8B5DR85qA3XhxRdLGD87c6rZTKO4cMcXV1u/8XoQVu1bo/nViu/zGDydLC+FsTwaLaHyu8zFzsDGcMLFvjx1jIRXaFK6eKmIRonD1dKyU6ZUKKRMRLYTTZqgkpUC4HoG10SvhWIKPfCWEa3VWpsYBoJC5ooj8K8cCLA2bI9SDzESI4ktt7Y5Pgth/OSTkbM9H9+Gc+u9jZ8TtWc3Z9Qhwm0sb6JZYWdZoXifsRrx8RES3201OkEjf3yRBjRS3Mnx4VRwjGUlSx7AN2p8eHybH/AY2cvbgfD6Ywt587cs/Fj1wk4L1Ud7pw4ZXZgyezIrlRgZyTDFSA1SDMLfC1ZBEOPv/Ykda5sb9mp5Krc74tUHTNgmPETdyqjeoG4DfsQJDyKbFxf4xWFjDtub7O3xcgh/K9x0Mzhwa6fg9/3PMtuTLiM/maHGDcGOiXxgNOgV/PndmBAIudYNtDNo2tl73',
+    'NID_JKL': 'X3eqv/dQvlG7V43qKvE97W/5W5rCxOPn3kYr5LD1Ok4=',
+    'page_uid': 'iJIHjsqVJLhssFFtjEdssssstEw-072672',
+    'nhn.realestate.article.rlet_type_cd': 'A01',
+    'nhn.realestate.article.trade_type_cd': '""',
+    'nhn.realestate.article.ipaddress_city': '4100000000',
+    '_fwb': '142CyTTa1N6DnA2DGtHCGAn.1740552557653',
+    'landHomeFlashUseYn': 'Y',
+    '_fwb': '142CyTTa1N6DnA2DGtHCGAn.1740552557653',
+    'REALESTATE': 'Wed%20Feb%2026%202025%2016%3A35%3A33%20GMT%2B0900%20(Korean%20Standard%20Time)',
+    'SRT30': '1740555334',
+    'SRT5': '1740555334',
+    'BUC': 'TNXfa1-Y6ifacNS_qpPLwTtmnrJg6K_Nvmbyits4tRE=',
+}
+
+header = {
+    'accept': '*/*',
+    'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlJFQUxFU1RBVEUiLCJpYXQiOjE3NDA1NTUzMzMsImV4cCI6MTc0MDU2NjEzM30.GPP-zf9HLe5hh8v1A1b4CBX0ZbS4KixhErE5xaJd4g0',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'priority': 'u=1, i',
+    'referer': 'https://new.land.naver.com/interests',
+    'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+}
+
+
+# 계양 이편한세상
+gaeyang = "https://new.land.naver.com/api/articles/complex/125101?realEstateType=APT%3AABYG%3AJGC%3APRE&tradeType=A1&tag=%3A%3A%3A%3A%3A%3A%3A%3A%3A&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=900000000&areaMin=0&areaMax=900000000&oldBuildYears&recentlyBuildYears&minHouseHoldCount&maxHouseHoldCount&showArticle=false&sameAddressGroup=false&minMaintenanceCost&maxMaintenanceCost&page=1&complexNo=125101&buildingNos=&areaNos=1&type=list&order=rank"
